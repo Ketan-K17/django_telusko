ae1c07949c2c30b596739a2ea513afaa4970be4d
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2025-03-05 16:45:42 +0530

    L8: learnt GET vs POST, why form-filling should take POST method (so that you don't see fields on address bar), and info retrieval must be GET

--- django_telusko/projects/first/calc/views.py
+++ django_telusko/projects/first/calc/views.py
@@ -7,7 +7,7 @@ def home(request):
 
 
 def add(request):
-    num1 = int(request.GET["num1"])
-    num2 = int(request.GET["num2"])
+    num1 = int(request.POST["num1"])
+    num2 = int(request.POST["num2"])
     res = num1 + num2
     return render(request, 'result.html', {'result': res}) 
