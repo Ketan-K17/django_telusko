fa93fe8a73133c9b1fa8dedf7c66b7d0831e2634
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2025-03-05 15:34:09 +0530

    L5: Learnt how to return dynamic html files from views instead of single Http responses, using DTL

--- django_telusko/projects/first/first/settings.py
+++ django_telusko/projects/first/first/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for first project.
 
@@ -54,7 +56,7 @@ ROOT_URLCONF = "first.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
