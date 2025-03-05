5025dd4c7f9e16bf510a9d45bef9edc7a816b4c5
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2025-03-05 16:16:29 +0530

    L7: doing basic computation using Django, and outputting to html. Understood how to process data given by website, and then output processed data to the website

--- django_telusko/projects/first/calc/views.py
+++ django_telusko/projects/first/calc/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name': 'Nidhi'}) 
+
+
+def add(request):
+    num1 = int(request.GET["num1"])
+    num2 = int(request.GET["num2"])
+    res = num1 + num2
+    return render(request, 'result.html', {'result': res}) 
