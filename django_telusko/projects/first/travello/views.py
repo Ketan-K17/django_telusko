f3cc05cbc30516f6c4b18c0fc107b5dde8590770
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2025-03-07 12:09:07 +0530

    L10: decided to work with a template website from internet, incorporating travello site in an app. Understood the refactors that need to be made for each new app in a bigger website project.

---
+++ django_telusko/projects/first/travello/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html') 
+
+
+def add(request):
+    num1 = int(request.POST["num1"])
+    num2 = int(request.POST["num2"])
+    res = num1 + num2
+    return render(request, 'result.html', {'result': res}) 
