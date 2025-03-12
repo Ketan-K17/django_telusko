17a57fa822272fb79de6732edb932d2ddca0cad4
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2025-03-12 13:10:19 +0530

    L11: ended up creating the travello site. It's all about importing the static files the right way through settings.py, and then changing all the hrefs to link to the static file instead, and putting the necessary html files, inside the templates folder. something a copilot can do.

--- django_telusko/projects/first/travello/urls.py
+++ django_telusko/projects/first/travello/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('destination/', views.destination, name='destination'),
+    path('destination_details/', views.destination_details, name='destination_details'),
+    path('elements/', views.elements, name='elements'),
+    path('blog/', views.blog, name='blog'),
+    path('single_blog/', views.single_blog, name='single_blog'),
+    path('contact/', views.contact, name='contact'),
 ] 
--- django_telusko/projects/first/travello/views.py
+++ django_telusko/projects/first/travello/views.py
@@ -5,6 +5,26 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html') 
 
+def about(request):
+    return render(request, 'about.html')
+
+def destination(request):
+    return render(request, 'travel_destination.html')
+
+def destination_details(request):
+    return render(request, 'destination_details.html')
+
+def elements(request):
+    return render(request, 'elements.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def single_blog(request):
+    return render(request, 'single-blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
 
 def add(request):
     num1 = int(request.POST["num1"])
