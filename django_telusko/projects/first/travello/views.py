443e08313da475f312af3b05ffaa866e8adaba14
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2025-03-18 21:24:10 +0530

    L13: further knowledge in DTL, and how i can use it to add dynamic images to templates

--- django_telusko/projects/first/travello/views.py
+++ django_telusko/projects/first/travello/views.py
@@ -6,12 +6,25 @@ from .models import Destination
 
 def index(request):
     dest1 = Destination()
-    dest1.id = 1
     dest1.name = "Mumbai"
     dest1.description = "The city that never sleeps"
-    dest1.image = "destination_1.jpg"
+    dest1.image = "mumbai.png"
     dest1.price = 700
-    return render(request, 'index.html', {'dest1': dest1}) 
+
+    dest2 = Destination()
+    dest2.name = "Margao"
+    dest2.description = "The city that's always half-asleep."
+    dest2.image = "margao.png"
+    dest2.price = 500
+
+    dest3 = Destination()
+    dest3.name = "Panaji"
+    dest3.description = "capital of Goa."
+    dest3.image = "panaji.png"
+    dest3.price = 600
+
+    dests = [dest1, dest2, dest3]
+    return render(request, 'index.html', {'dests': dests}) 
 
 def about(request):
     return render(request, 'about.html')
