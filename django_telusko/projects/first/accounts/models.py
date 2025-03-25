6caf0647a748de4912dcd43819461697bd94c16c
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2025-03-25 12:08:14 +0530

    L22: Created registration page.

--- django_telusko/projects/first/accounts/models.py
+++ django_telusko/projects/first/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    confirm_password = models.CharField(max_length=100)
--- django_telusko/projects/first/accounts/views.py
+++ django_telusko/projects/first/accounts/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+# this is the user's model. You don't need to create it manually, just use this.
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        user_name = request.POST['user_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            if User.objects.filter(username=user_name).exists():
+                print("Username already exists")
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                print("Email already exists")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(first_name=first_name, last_name=last_name, username=user_name, email=email, password=password)
+                user.save()
+                print("User created")
+                return redirect('login')
+        else:
+            print("Passwords do not match")
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
 
 def login(request):
     return render(request, 'login.html')
